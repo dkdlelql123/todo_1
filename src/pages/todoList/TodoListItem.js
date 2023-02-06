fd1d0bd6f20d22322b27e17a73558f5cd729d389
NamYeJi <dkdlelql123@naver.com>
2023-02-07 05:35:45 +0900

    수정모드로 변경하는 버튼

--- src/pages/todoList/TodoListItem.js
+++ src/pages/todoList/TodoListItem.js
@@ -1,16 +1,37 @@
 import { useRef, useState } from "react";
 
 function TodoListItem({ todo, index, todosState }) {
+  const [editMode, setEditMode] = useState(false);
+
   const onClickRemoveItem = () => todosState.removeTodo(index);
+  const onClickModifyItem = () => {
+    setEditMode(true);
+  };
+
   return (
     <>
       <li className="flex gap-2">
         <span>{todo.id}</span>
-        <span>{todo.regDate}</span>
-        <span>{todo.content}</span>
-        <button className="btn" onClick={onClickRemoveItem}>
-          삭제
-        </button>
+
+        {editMode == false ? (
+          <>
+            <span>{todo.regDate}</span>
+            <span>{todo.content}</span>
+            <button className="btn" onClick={onClickModifyItem}>
+              수정
+            </button>
+            <button className="btn" onClick={onClickRemoveItem}>
+              삭제
+            </button>
+          </>
+        ) : (
+          <>
+            <span>{todo.content}</span>
+            <button className="btn" onClick={onClickModifyItem}>
+              수정완료
+            </button>
+          </>
+        )}
       </li>
     </>
   );
