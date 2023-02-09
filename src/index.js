2f945bc37b4f69745a0fba9b5c6e4325021d4fe7
NamYeJi <dkdlelql123@naver.com>
2023-02-10 05:49:17 +0900

    reset css 설정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Root from './Root';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Root from "./Root";
+import { CssBaseline } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <Root />
   </React.StrictMode>
 );
